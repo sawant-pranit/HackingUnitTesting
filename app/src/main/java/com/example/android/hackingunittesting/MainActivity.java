d37e75b1f86f437f56da903d5e875524df670895
pranit <pranitsawant01@gmail.com>
2017-07-26 09:24:07 +0530

    added espresso test

--- app/src/main/java/com/example/android/hackingunittesting/MainActivity.java
+++ app/src/main/java/com/example/android/hackingunittesting/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.android.hackingunittesting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.inputField);
+    }
+
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.changeText:
+                editText.setText("Lalala");
+                break;
+            case R.id.switchActivity:
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("input", editText.getText().toString());
+                startActivity(intent);
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/android/hackingunittesting/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.hackingunittesting;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView viewById = (TextView) findViewById(R.id.resultView);
+        Bundle inputData = getIntent().getExtras();
+        String input = inputData.getString("input");
+        viewById.setText(input);
+    }
+}
